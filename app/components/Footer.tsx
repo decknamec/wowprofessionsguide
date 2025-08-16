54bf8ff34b24540f957b1b40111b8bf5722df1c5
Moritz <decknamec@gmail.com>
2025-08-16 12:34:15 +0200

    Footer

---
+++ app/components/Footer.tsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+  return (
+    <footer className="absolute inset-x-0 bottom-5 text-center text-[11px] text-white/50">
+      Letztes Update: 16. Aug 2025 · © Deine Seite
+    </footer>
+  );
+}
